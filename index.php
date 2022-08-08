730ad2660a748566daf848b491d584280e753dfd
Eazy Web Philippines <92870706+eazywebph@users.noreply.github.com>
2022-08-08 17:01:16 +0800

    .

--- index.php
+++ index.php
@@ -4,29 +4,108 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Mailblaze Toolkit 1.0</title>
+    <title>Mailblaze Tools</title>
     
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
+    <!-- Bootstrap JS Bundle (including Popper) -->
+    <!-- Production
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+     -->
+    <!-- Development -->
+    <script src="js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+    <!-- Global CSS overrides -->
+    <link rel="stylesheet" href="css/global.css">
+
+    <!-- Icons Library -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
     <link rel="icon" type="image/x-icon" href="favicon.ico">
   </head>
   <body>
 
-  <!-- Container samples for reference (to be removed at production)
-  <div class="container-sm">100% wide until small breakpoint</div>
-  <div class="container-md">100% wide until medium breakpoint</div>
-  <div class="container-lg">100% wide until large breakpoint</div>
-  <div class="container-xl">100% wide until extra large breakpoint</div>
-  <div class="container-xxl">100% wide until extra extra large breakpoint</div>
-    
-  <div class="container-fluid">
-  Fluid Container
-  </div>
-  -->
+    <!-- Navbar and Container Div start -->
+    <nav class="navbar navbar-dark bg-dark sticky-top">
+    <div class="container">
+
+      <!-- Logo left + Brand -->
+      <a class="navbar-brand" href="#"><i class="bi bi-tools"></i> Mailblaze Tools</a>
+
+      <!-- Main ul start -->
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Admin
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Generate Report</a></li>
+            <li><a class="dropdown-item" href="#">Request Delisting</a></li>
+            <li><a class="dropdown-item" href="#">IP Addresses</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Campaigns
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Overview</a></li>
+            <li><a class="dropdown-item" href="#">Analyze Draft</a></li>
+            <li><a class="dropdown-item" href="#">Analyze Sent</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Lists
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Overview</a></li>
+            <li><a class="dropdown-item" href="#">Manage</a></li>
+            <li><a class="dropdown-item" href="#">Check CSV</a></li>
+            <li><a class="dropdown-item" href="#">Import</a></li>
+            <li><a class="dropdown-item" href="#">Export</a></li>
+          </ul>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Support</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Docs</a>
+        </li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="bi bi-person-circle"></i>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Jasmine Trias</a></li><hr>
+            <li><a class="dropdown-item" href="#">Manage Profile</a></li>
+            <li><a class="dropdown-item" href="#">Account Settings</a></li>
+            <li><a class="dropdown-item" href="#">Payment</a></li><hr>
+            <li class="nav-item"><a class="nav-link" href="#">Settings&nbsp;<i class="bi bi-sliders"></i></a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Logout&nbsp;<i class="bi bi-box-arrow-right"></i></a></li>
+          </ul>
+        </li>
+
+      </ul>
+      <!-- Main ul end -->
+    </div>
+    </nav>
+    <!-- Navbar and Container div ends -->
+
+
+
+
+ 
 
   </body>
 </html>
