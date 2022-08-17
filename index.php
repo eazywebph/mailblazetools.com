758d72e8ba023cb62452ef5ebbd1a30cdc60f5ec
Eazy Web Philippines <92870706+eazywebph@users.noreply.github.com>
2022-08-17 20:57:15 +0800

    .

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+
     <title>Mailblaze Tools</title>
-    
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
@@ -19,88 +19,71 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
     <link rel="icon" type="image/x-icon" href="favicon.ico">
-  </head>
-  <body>
+</head>
+<body>
 
     <!-- Navbar and Container Div start -->
     <nav class="navbar navbar-dark bg-dark sticky-top">
-    <div class="container">
+        <div class="container">
+    
+          <!-- Logo left + Brand -->
+          <a class="navbar-brand" href="#"><i class="bi bi-tools"></i> Mailblaze Tools</a>
+    
+          <!-- Main ul start -->
+          <ul class="nav">
+            <!--
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Tools
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Check Spam Words</a></li>
+                <li><a class="dropdown-item" href="#">Delist Helper</a></li>
+                <li><a class="dropdown-item" href="#">Bulk Email Checker</a></li>
+              </ul>
+            </li>
 
-      <!-- Logo left + Brand -->
-      <a class="navbar-brand" href="#"><i class="bi bi-tools"></i> Mailblaze Tools</a>
+    
+            <li class="nav-item">
+              <button type="button" class="btn btn-primary">Sign Up</button>
+            </li>
 
-      <!-- Main ul start -->
-      <ul class="nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
+            &nbsp;&nbsp;
 
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Admin
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Generate Report</a></li>
-            <li><a class="dropdown-item" href="#">Request Delisting</a></li>
-            <li><a class="dropdown-item" href="#">IP Addresses</a></li>
-          </ul>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Campaigns
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Overview</a></li>
-            <li><a class="dropdown-item" href="#">Analyze Draft</a></li>
-            <li><a class="dropdown-item" href="#">Analyze Sent</a></li>
-          </ul>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Lists
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Overview</a></li>
-            <li><a class="dropdown-item" href="#">Manage</a></li>
-            <li><a class="dropdown-item" href="#">Check CSV</a></li>
-            <li><a class="dropdown-item" href="#">Import</a></li>
-            <li><a class="dropdown-item" href="#">Export</a></li>
-          </ul>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Docs</a>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="bi bi-person-circle"></i>&nbsp; Jasmine Trias
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">View Profile</a></li><hr>
-            <li><a class="dropdown-item" href="#">Manage Profile</a></li>
-            <li><a class="dropdown-item" href="#">Account Settings</a></li>
-            <li><a class="dropdown-item" href="#">Subscription</a></li>
-            <li><a class="dropdown-item" href="#">Payment</a></li><hr>
-
-            <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-sliders"></i>&nbsp;Settings</a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-question-circle"></i>&nbsp;Help</a></li>
-            <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-box-arrow-right"></i>&nbsp;Logout</a></li>
-          </ul>
-        </li>
-
-      </ul>
-      <!-- Main ul end -->
+            <li class="nav-item">
+              <button type="button" class="btn btn-secondary">Login</button>
+            </li>
+            -->
+        </ul>
+        <!-- Main ul end -->
     </div>
     </nav>
     <!-- Navbar and Container div ends -->
 
-    <div class="container">
-      
+    <br><br><br>
+
+    <div class="container text-center">
+      <div class="row justify-content-md-center">
+        <h3>
+          Spam Words Checker<br>
+          <small class="text-muted">Copy all the text from the top (after the header) down to the bottom (before the footer).<br>Paste it in the text box below and click Analyze, see the results on the right.</small>
+        </h3>
+      </div>
+      <br>
+      <div class="row justify-content-md-center">
+        <div class="col-7">
+          <textarea id="w3review" name="w3review" rows="12" cols="100%"></textarea>
+          <input type="button" value="Analyze">
+        </div>
+
+        <div class="col-1"></div>
+
+        <div class="col-4">col-4</div>
+      </div>
     </div>
+
+    
  
 
-  </body>
+</body>
 </html>
